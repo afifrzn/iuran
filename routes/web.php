b45b736aea1c5eea1e3f743712c5beb13efe74be
Afif-1002 <afifrahmanzian02@gmail.com>
2026-05-05 18:40:35 +0700

    fix api

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
-<?php
+// <?php
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ParticipantController;
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ParticipantController;
-
-Route::get('/participants', [ParticipantController::class, 'index']);
-Route::post('/participants', [ParticipantController::class, 'store']);
-Route::post('/participants/{id}/upgrade', [ParticipantController::class, 'upgrade']);
-Route::post('/participants/{id}/bayar', [ParticipantController::class, 'bayar']);   
+// Route::get('/participants', [ParticipantController::class, 'index']);
+// Route::post('/participants', [ParticipantController::class, 'store']);
+// Route::post('/participants/{id}/upgrade', [ParticipantController::class, 'upgrade']);
+// Route::post('/participants/{id}/bayar', [ParticipantController::class, 'bayar']);   
