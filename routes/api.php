528388e720a8dc822ba7fce29f3c27fad2619557
Afif-1002 <afifrahmanzian02@gmail.com>
2026-05-05 18:01:08 +0700

    add middleware

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ParticipantController;
-
-Route::get('/participants', [ParticipantController::class, 'index']);
-Route::post('/participants', [ParticipantController::class, 'store']);
-Route::post('/participants/{id}/upgrade', [ParticipantController::class, 'upgrade']);
-Route::post('/participants/{id}/bayar', [ParticipantController::class, 'bayar']);
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use App\Http\Controllers\ParticipantController;
 Route::get('/participants', [ParticipantController::class, 'index']);
 Route::post('/participants', [ParticipantController::class, 'store']);
 Route::post('/participants/{id}/upgrade', [ParticipantController::class, 'upgrade']);
-Route::post('/participants/{id}/bayar', [ParticipantController::class, 'bayar']);
+Route::post('/participants/{id}/bayar', [ParticipantController::class, 'bayar']);   
