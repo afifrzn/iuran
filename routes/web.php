6452c9852f87df7ef3a41f8190d9896de76eda3d
Afif-1002 <afifrahmanzian02@gmail.com>
2026-05-05 17:44:45 +0700

    add api2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ParticipantController;
 
 Route::get('/participants', [ParticipantController::class, 'index']);
 Route::post('/participants', [ParticipantController::class, 'store']);
